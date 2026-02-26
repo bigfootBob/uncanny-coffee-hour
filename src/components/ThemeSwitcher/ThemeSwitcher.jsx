cc4978c6311186de6877526c22941637455abe7d
Robert Mason <bob.j.mason@gmail.com>
2026-02-26 13:22:54 -0800

    default to dark no matter what

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -5,7 +5,7 @@ const ThemeSwitcher = () => {
   const [theme, setTheme] = useState(() => {
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme) return savedTheme;
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    return 'dark';
   });
 
   useEffect(() => {
